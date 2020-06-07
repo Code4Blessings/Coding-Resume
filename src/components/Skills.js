1d4e41f775c96ec20959d106524ff0dc30410189
Code4Blessings <robinwarden@code4blessings.com>
2020-06-07 09:51:45 -0400

    Added node-sass dev dependency

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,9 +4,11 @@ const Skills = () => {
     return (
         <div className="skills-container">
             <div className="section-title">
-            
+                <h3>Skills</h3>
             </div>
-            
+            <div className="section-list">
+                HTML, CSS, Git, JavaScript, React Hooks and Class Components, Node.js, Context API, Mock, Unit, and Integration Testing, Axios, GraphQL, Prisma, Apollo, Authentication, Redux
+            </div>
         </div>
     );
 };
