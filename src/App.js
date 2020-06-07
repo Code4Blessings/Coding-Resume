d10144565d2483d400d676199b5666078d7b2907
Code4Blessings <robinwarden@code4blessings.com>
2020-06-07 09:34:36 -0400

    Added more components and changed yarn to npm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Header from './components/Header'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
+import Experience from './components/Experience'
+import Education from './components/Education'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Skills />
+      <Projects />
+      <Experience />
+      <Education />
     </div>
   );
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Skills = () => {
     return (
-        <div>
+        <div className="skills-container">
+            <div className="section-title">
+            
+            </div>
             
         </div>
     );
