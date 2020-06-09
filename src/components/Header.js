90f55f78106690b2e76d99360eaf6a1b9953e6e5
Code4Blessings <robinwarden@code4blessings.com>
2020-06-09 00:31:20 -0400

    Updated scss files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import Contact from './Contact'
 
 const Header = () => {
     return (
-        <div className="head-container">
-            <div className="title">
+        <div className="header-container">
+            <div className="section-title">
                 <h1>Robin <span>Warden</span></h1>
-                <h1>Software Developer</h1>
-                <Contact />
             </div>
-            
+            <div className="role">
+                <h1>Software Developer</h1>
+            </div>
+               <div className="contact">
+                    <p>Lawrenceville, GA</p>
+                <div className="links">
+                    <a href="robincw35@gmail.com">robincw35@gmail.com</a>
+                    <a href="https://code4blessings.github.io/RobinWarden_Portfolio/">
+                        My Website
+                    </a>
+                    <a href="https://github.com/Code4Blessings">GitHub</a>
+                    <a href="https://www.linkedin.com/in/robin-warden-7126b054">linkedin
+                    </a>
+                </div>
+            </div>
         </div>
-       
     );
 };
 
